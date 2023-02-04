2ca90c389d505611950073342224cbd499790175
JulietteT <Ihbrbh19@mail.ru>
2023-02-04 17:17:01 +0500

    initial commit

--- src/test/data/DataGenerator.java
+++ src/test/data/DataGenerator.java
@@ -12,46 +12,30 @@ import java.util.Random;
 public class DataGenerator {
     private DataGenerator() {
     }
+    private static final Faker faker = new Faker(new Locale("ru"));
 
+    @Step("Генерируем актуальную дату с запасом в 4 дня в формате д.м.г")
     public static String generateDate(int shift) {
-        // TODO: добавить логику для объявления переменной date и задания её значения, для генерации строки с датой
-        // Вы можете использовать класс LocalDate и его методы для получения и форматирования даты
-        return date;
+        return LocalDate.now().plusDays(4).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
     }
 
-    public static String generateCity(String locale) {
-        // TODO: добавить логику для объявления переменной city и задания её значения, генерацию можно выполнить
-        // с помощью Faker, либо используя массив валидных городов и класс Random
-        return city;
+    @Step("Фейкеом генерируем рандомные города")
+    public static String generateCity() {
+        return faker.address().cityName();
     }
 
-    public static String generateName(String locale) {
-        // TODO: добавить логику для объявления переменной name и задания её значения, для генерации можно
-        // использовать Faker
-        return name;
+    @Step("Фейкеом генерируем рандомные имена")
+    public static String generateName() {
+        return faker.name().fullName();
     }
 
-    public static String generatePhone(String locale) {
-        // TODO: добавить логику для объявления переменной phone и задания её значения, для генерации можно
-        // использовать Faker
-        return phone;
+    @Step("Фейкеом генерируем рандомные номера телефонов из 11 цифр")
+    public static String generatePhone() {
+        return faker.number().digits(11);
     }
 
-    public static class Registration {
-        private Registration() {
-        }
-
-        public static UserInfo generateUser(String locale) {
-            // TODO: добавить логику для создания пользователя user с использованием методов generateCity(locale),
-            // generateName(locale), generatePhone(locale)
-            return user;
-        }
-    }
-
-    @Value
-    public static class UserInfo {
-        String city;
-        String name;
-        String phone;
+    @Step("Генерируем всю рандомную информацию о пользователе: город, имя, телефон")
+    public static UserInfo generateUser(String locale) {
+        return new UserInfo(generateCity(), generateName(), generatePhone());
     }
 }
